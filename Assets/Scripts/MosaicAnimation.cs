fa4f73ce510c4ebdd5d5b459869591bcdbc7e5fe
Keijiro Takahashi <keijiro@gmail.com>
2024-02-09 11:30:59 +0900

    Use IMaterialModifier

--- Assets/Scripts/MosaicAnimation.cs
+++ Assets/Scripts/MosaicAnimation.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public sealed class MosaicAnimation : MonoBehaviour
+public sealed class MosaicAnimation : MonoBehaviour, IMaterialModifier
 {
-    Material _material;
     float _param;
     bool _reveal;
 
     public void Toggle() => _reveal = !_reveal;
 
-    Image Target => GetComponent<Image>();
-
-    void Start()
-      => Target.material = _material = new Material(Target.material);
-
     void Update()
     {
-        _param = Mathf.Clamp01(_param + Time.deltaTime * (_reveal ? 1 : -1));
+        _param += Time.deltaTime * (_reveal ? 1 : -1);
+        if (_param >= 0 && _param <=1) GetComponent<Image>().SetMaterialDirty();
+        _param = Mathf.Clamp01(_param);
+    }
+
+    public Material GetModifiedMaterial(Material baseMaterial)
+    {
         var res = Mathf.Lerp(8, 512, Mathf.Pow(_param, 4));
-        Target.materialForRendering.SetFloat("_Resolution", res);
+        baseMaterial.SetFloat("_Resolution", res);
+        return baseMaterial;
     }
 }
